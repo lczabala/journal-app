5793a732a57e1dda64eca1a7c086d3ea1800eae2
Luis Zabala <lzabala28@gmail.com>
2020-09-03 22:52:59 -0400

    se empieza a dar estilos a login

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,7 +3,27 @@ import React from 'react'
 export const LoginScreen = () => {
     return (
         <div>
-            <h1>Login Screen</h1>
+            <h3>Login</h3>
+            <form>
+                <input 
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                />
+
+                <input 
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                />
+                <button
+                    type="submit"
+                >
+                    Login
+                </button>
+
+                google
+            </form>
         </div>
     )
 }
