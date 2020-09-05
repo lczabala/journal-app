07028ed3bf75f7737f002fb2d49ab08fb4c5e883
Luis Zabala <lzabala28@gmail.com>
2020-09-05 13:03:02 -0400

    se agregan estilos al register

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegisterScreen = () => {
     return (
         <div>
-            <h1>Register Screen</h1>
+            <h3 className="auth__title">Create Account</h3>
+            <form>
+                <input 
+                    className="auth__input"
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    autoComplete="none"
+                />
+                <input 
+                    className="auth__input"
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    autoComplete="none"
+                />
+                <input 
+                    className="auth__input"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                />
+                <input 
+                    className="auth__input"
+                    type="password"
+                    placeholder="Confirm Password"
+                    name="confirmpassword"
+                />
+                <button
+                    className="button__btn button__primary button__block mb-5"
+                    
+                    type="submit"
+                >
+                    Join us!
+                </button>
+                
+
+                <Link to="/auth/login" className="links">
+                    Alredy register?
+                </Link>                
+            </form>
         </div>
     )
 }
