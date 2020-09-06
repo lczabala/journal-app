bcfe47ce2c42c605707e31928f2c3ef67b6afc32
Luis Zabala <lzabala28@gmail.com>
2020-09-06 19:15:06 -0400

    listo diseño

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,17 @@ export const NoteScreen = () => {
     return (
         <div className="note__main-content">
             <NoteAppBar/>
+
+            <div className="note__content">
+                <form>
+                    <input type="text" placeholder="Title" className="note__input note__title-input"/>
+                    <textarea placeholder="What happend today?" className="note__input note__textarea"/>
+                </form>
+            </div>
+
+            <div className="note__image">
+                <img src="https://cdn.pixabay.com/photo/2016/11/29/04/13/architecture-1867262_960_720.jpg" alt="image"/>
+            </div>
         </div>
     )
 }
